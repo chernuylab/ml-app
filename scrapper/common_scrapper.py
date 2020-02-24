348fbc49a3bf7776ac044c20a11697f1f2b44140
Murad Adygezalov <murad@MacBook-Pro-Murad-2.local>
2020-02-24 22:51:20 +0300

    Added new files.

--- scrapper/common_scrapper.py
+++ scrapper/common_scrapper.py
@@ -1,21 +1,27 @@
 from urllib.request import urlretrieve
 from urllib.request import urlopen
+from urllib.parse import quote
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from methods import Methods as m
 import os
 
 
-html = urlopen("https://yandex.ru/images/search?from=tabbar&text=балетки%20женские").decode('utf-8')
-print(html.read().decode('utf-8'))
-#bsObj = BeautifulSoup(html, "html.parser")
+path = 'https://yandex.ru'
+m.make_sure_path_exists('Yandex')
+html = urlopen(path+'/images/search?from=tabbar&text='+quote('балетки%20женские'))
+bsObj = BeautifulSoup(html, "html.parser")
 
-#
-# j = 1
-# for link in bsObj.find("div", {"class":"serp-controller__content"}).find("div", {"class":"serp-list"}).findAll("div", {"class":"serp-item"}):
-#     img_link = link.find("div", {"class": "serp-item__preview"}).find("a", {"class": "serp-item__link"})
-#     print(str(img_link.attrs['href']))
+j = 1
+for link in bsObj.find("div", {"class":"serp-controller__content"}).find("div", {"class":"serp-list"}).findAll("div", {"class":"serp-item"}):
+    img_link = link.find("div", {"class": "serp-item__preview"}).find("a", {"class": "serp-item__link"}).find("img", {"class": "serp-item__thumb"})
 
+    m.make_sure_path_exists('Yandex/Балетки')
+    if 'src' in img_link.attrs:
+        #print('https:'+img_link.attrs['src'])
+        for i in range(0-999):
+            print('https:'+img_link.attrs['src'], m.getImagesDir()+'Yandex/Балетки/Балетки'+'.'+str(j)+'.jpg')
+            j += 1
     # if 'data-gallery' in link.attrs:
     #     category_link = link.attrs['data-gallery'].strip('[').strip(']').strip('"')
     #     r = category_link.split('" , "')
